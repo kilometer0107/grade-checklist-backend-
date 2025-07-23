ab03138f3af24c46761783b28977dcf4635976cc
김재승 <aearaear@kookmin.ac.kr>
2025-07-23 21:40:36 +0900

    initial commit

---
+++ src/main/java/com/team/grade_checklist/GradeChecklistApplication.java
@@ -0,0 +1,13 @@
+package com.team.grade_checklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeChecklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeChecklistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/grade_checklist/GradeChecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.grade_checklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeChecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
