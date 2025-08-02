1d60409d37cc6e3f289539130f5e5f0f0c91074a
Jeongdaeun1 <daeun1q2w3e@gmail.com>
2025-08-02 23:41:36 +0900

    repository랑 service 파일 생성

--- src/main/java/com/team/grade_checklist/domain/user/controller/UserController.java
+++ src/main/java/com/team/grade_checklist/domain/user/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.team.grade_checklist.domain.user.controller;
 
+import com.team.grade_checklist.domain.user.dto.response.SignupResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<SignupResponse>
 }
---
+++ src/main/java/com/team/grade_checklist/domain/user/service/UserService.java
@@ -0,0 +1,16 @@
+package com.team.grade_checklist.domain.user.service;
+
+
+import com.team.grade_checklist.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    
+}
