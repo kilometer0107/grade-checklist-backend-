f584cea60493a621536c1bddd8a22e9b7a507054
MinJuTur <jmj4701jmj@gmail.com>
2025-08-20 22:45:17 +0900

    refactor: 입학년도 범위 2022~2025로 수정

--- src/main/java/com/team/grade_checklist/domain/user/dto/request/RegisterRequest.java
+++ src/main/java/com/team/grade_checklist/domain/user/dto/request/RegisterRequest.java
@@ -28,7 +28,7 @@ public class RegisterRequest {
     private String department;
 
     @NotNull(message = "입학년도는 필수입니다")
-    @Min(value = 2000, message = "입학년도는 2000년 이후여야 합니다")
-    @Max(value = 2030, message = "입학년도는 2030년 이전이어야 합니다")
+    @Min(value = 2022, message = "입학년도는 2022년 이후여야 합니다")
+    @Max(value = 2025, message = "입학년도는 2025년 이전이어야 합니다")
     private Integer admissionYear;
 }
