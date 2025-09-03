f49635d936fe7734f82f863f67428a9c7ee3f959
Jeongdaeun1 <daeun1q2w3e@gmail.com>
2025-09-03 16:49:02 +0900

    subject 엔티티

---
+++ src/main/java/com/team/grade_checklist/domain/subjects/entity/Subjects.java
@@ -0,0 +1,30 @@
+package com.team.grade_checklist.domain.subjects.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="subjects")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Subjects {
+    @Id
+    @Column(name = "subjects_id", nullable = false)
+    @GeneratedValue
+    private Integer subjectId;
+
+    @ManyToOne
+    @JoinColumn(name = "course_classification_id", nullable = false)
+    private Integer courseId;
+
+    private String subjectName;
+
+    private Integer credit;
+
+    private String grade;
+
+    private String semester;
+}
---
+++ src/main/java/com/team/grade_checklist/domain/subjects/enums/Major.java
@@ -0,0 +1,4 @@
+package com.team.grade_checklist.domain.subjects.enums;
+
+public class Major {
+}
